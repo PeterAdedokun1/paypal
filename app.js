5d3ab99eeecb7c184a6d91b5c310714261a19d47
Peter Opeyemi <92626192+PeterAdedokun1@users.noreply.github.com>
2022-04-20 09:32:45 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+// const nav1 = document.querySelector(".nav-1");
+// const display = document.querySelector(".display");
+// const paypal = document.querySelector(".paypal")
+// const closeBtn = document.querySelector(".close-btn");
+// const select = document.querySelector(".select");
+// const hi = document.querySelector(".hi")
+// console.log(select)
+// // console.log(paypal)
+// const underline = document.querySelector(".underline");
+
+
+// nav1.addEventListener("mouseenter", function () {
+//         display.style.display = "block"
+// })
+// nav1.addEventListener("click", function () {
+//     if (display.classList.contains("paypal")) {
+//         display.style.display = "none"
+//     }
+//     if (display.classList.remove("display")) {
+//        display.style.display = "none"
+//      }
+// })
+// closeBtn.addEventListener("click", function () {
+//     display.style.display = "none"
+
+// })
